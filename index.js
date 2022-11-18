8a288a11af7c0603ccded7284f4ae4107835c561
Emilio Parra <Emilio.pg95@gmail.com>
2022-11-17 18:54:12 -0600

    🚧 Switch generated

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 /* window.onload: The load event is fired when the whole page has loades, inlcuding
 all dependent resources such as stylesheets and images */
 window.onload = function(){
-document.getElementById('swithToReg').onclick=swithToReg
+document.getElementById('switchToReg').onclick=switchToReg
+document.getElementById('switchToLogin').onclick=switchToLogin
+
 
 // Switch to register
 function switchToReg() {
     document.getElementById('register-portal').style = "display:inline-block"
     document.getElementById('login-portal').style = "display:none"
-
     }
+
+// Switch to login
+    function switchToLogin() {
+        document.getElementById('register-portal').style = "display:none"
+        document.getElementById('login-portal').style = "display:inline-block"
+        }
+
+    
 }
