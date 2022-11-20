0ef7ce813594e1c7e193d8e1f9f415fbbd518f3f
Emilio Parra <Emilio.pg95@gmail.com>
2022-11-19 18:57:31 -0600

    ✨ Login and register validations done

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-/* window.onload: The load event is fired when the whole page has loades, inlcuding
+/* window.onload: The load event is fired when the whole page has loades, including
 all dependent resources such as stylesheets and images */
 window.onload = function(){
 document.getElementById('switchToReg').onclick=switchToReg
 document.getElementById('switchToLogin').onclick=switchToLogin
+document.getElementById('login-btn').onclick = loginValidation
+document.getElementById('register-btn').onclick = registerValidation
+
 
 
 // Switch to register
@@ -17,5 +20,31 @@ function switchToReg() {
         document.getElementById('login-portal').style = "display:inline-block"
         }
 
-    
+    //Account Number and Pin pattern
+    let AccNoPat = "^[0-9]{6}$"  //I only allow an account with a 6 digit number
+    let accPinPat = "^[0-9]{4}$" //I only allow a pin with a 4 digit number
+
+    // Login validation
+        function loginValidation(){
+            let lAccNo = document.getElementById('lAccNo').value
+            let lAccPin = document.getElementById('lAccPin').value
+            if(lAccNo.match(AccNoPat) && lAccPin.match(accPinPat)){
+                alert("welcome")
+            } else {
+                document.getElementById('errMsg').innerHTML = 'Enter a valid account or pin'
+            }
+        }
+
+    //Register validation
+    function registerValidation(){
+    let rAccName = document.getElementById('rAccName').value
+    let rAccNo = document.getElementById('rAccNo').value
+    let rAccPin = document.getElementById('rAccPin').value
+    let rConAccPin = document.getElementById('rConAccPin').value
+    if(rAccName!==null && rAccNo.match(AccNoPat) && rAccPin.match(accPinPat) && rAccPin == rConAccPin) {
+        alert("Registered")
+    } else {
+        alert("Please enter valid details")
+        }
+    }
 }
